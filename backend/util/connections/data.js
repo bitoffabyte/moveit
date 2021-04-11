e8563070671cf29151fd73c587dad5a504a2937b
eminguyen <emily@computer.org>
2021-04-11 04:25:19 -0700

    idk

--- backend/util/connections/data.js
+++ backend/util/connections/data.js
@@ -1,5 +1,6 @@
 module.exports = async (io, client) => {
   client.on('setExercise', data => {
+    console.log('set exercise');
     client.to(data.roomId).emit('newExercise', data.exercise);
   });
 };
