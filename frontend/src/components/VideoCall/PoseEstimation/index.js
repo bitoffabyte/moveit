3353be8ed211a7837f9f9ab3b6a9649c965370b0
eminguyen <emily@computer.org>
2021-04-10 18:55:47 -0700

    More

--- frontend/src/components/VideoCall/PoseEstimation/index.js
+++ frontend/src/components/VideoCall/PoseEstimation/index.js
@@ -6,6 +6,9 @@ import Sketch from 'react-p5';
 
 const minPoseConfidence = 0.2;
 
+const canvasWidth = window.innerHeight * 1.11;
+const canvasHeight = window.innerHeight * 0.83;
+
 const PoseEstimation = () => {
     const videoRef = useRef(); 
     const [poses, setPoses] = useState([]);    
@@ -65,7 +68,7 @@ const PoseEstimation = () => {
 	};
 
 	const draw = (p5) => {
-        p5.image(videoRef.current, 0, 0, window.innerHeight * 1.11, window.innerHeight * 0.83);
+        p5.image(videoRef.current, 0, 0, canvasWidth, canvasHeight);
 
         const squatCoords = {} 
 
@@ -80,8 +83,8 @@ const PoseEstimation = () => {
                 if (keypoint.score > minPoseConfidence) {
                     p5.fill(255, 255, 255);
                     p5.noStroke();
-                    const positionX = (keypoint.position.x / 640) * window.innerHeight * 1.11;
-                    const positionY = (keypoint.position.y / 480) *  window.innerHeight * 0.83;
+                    const positionX = (keypoint.position.x / 640) * canvasWidth;
+                    const positionY = (keypoint.position.y / 480) *  canvasHeight;
                     p5.ellipse(positionX, positionY, 15, 15);
 
                     if (keypoint.part === "leftKnee" || keypoint.part === "leftHip" || keypoint.part === "rightKnee" || keypoint.part === "rightHip") {
@@ -99,10 +102,10 @@ const PoseEstimation = () => {
             for (let j = 0; j < skeleton.length; j += 1) {
                 const partA = skeleton[j][0];
                 const partB = skeleton[j][1];
-                const positionXA = (partA.position.x / 640) * window.innerHeight * 1.11;
-                const positionYA = (partA.position.y / 480) *  window.innerHeight * 0.83;
-                const positionXB = (partB.position.x / 640) * window.innerHeight * 1.11;
-                const positionYB = (partB.position.y / 480) *  window.innerHeight * 0.83;
+                const positionXA = (partA.position.x / 640) * canvasWidth;
+                const positionYA = (partA.position.y / 480) *  canvasHeight;
+                const positionXB = (partB.position.x / 640) * canvasWidth;
+                const positionYB = (partB.position.y / 480) *  canvasHeight;
                 p5.strokeWeight(8);
                 p5.stroke(255, 255, 255);
                 p5.line( positionXA, positionYA, positionXB, positionYB);
