46540cde533d2c9df75990dc7531d1f2dd74fb79
Daniel Truong <ddtruong06@gmail.com>
2021-04-10 19:37:37 -0700

    Squats + curls

--- frontend/src/components/VideoCall/PoseEstimation/index.js
+++ frontend/src/components/VideoCall/PoseEstimation/index.js
@@ -1,44 +1,108 @@
 import React, { useRef, useState } from 'react'; 
-import { angle, checkSquatDown, checkSquatStanding } from './utils';
+import { angle, checkCurlActive, checkCurlRest, checkSquatDown, checkSquatStanding, average, findExercise, isSquatPosition } from './utils';
 
 import ml5 from 'ml5';
 import Sketch from 'react-p5';
 
 const minPoseConfidence = 0.2
 
+const squatJoints = ["leftKnee", "leftHip", "rightHip", "rightKnee"];
+const curlJoints = ["leftShoulder", "leftElbow", "rightShoulder", "rightElbow", "leftWrist", "rightWrist"];
+
 const PoseEstimation = () => {
     const videoRef = useRef(); 
     const [poses, setPoses] = useState([]);    
 
-    const [leftThighAngle, setLeftThighAngle] = useState(0);
-    const [rightThighAngle, setRightThighAngle] = useState(0);
-
     const [squatsCount, setSquatsCount] = useState(0);
+    const [curlsCount, setCurlsCount] = useState(0);
 
     // "", "up", "down"
-    const [squatsState, setSquatsState] = useState("");
+    const [squatsState, setSquatsState] = useState("up");
+
+    // "", "rest", "curl"
+    const [curlsState, setCurlsState] = useState("");
+
+
+    const [leftSquatAngle, setleftSquatAngle] = useState(0);
+    const [rightSquatAngle, setrightSquatAngle] = useState(0);
+
+    const [leftCurlAngle, setleftCurlAngle] = useState(0);
+    const [rightCurlAngle, setrightCurlAngle] = useState(0);
+
+    const [currentExercise, setCurrentExercise] = useState();
 
     const squatDetection = (squatCoords) => {
-        // look for squats (leftKnee --> leftHip, rightKnee --> rightHip, leftHip --> rightHip)
-        if (Object.keys(squatCoords).length !== 4) {
-            return
+        // look for squats
+        if (Object.keys(squatCoords).length !== squatJoints.length) {
+            // if (squatsState === "up") {
+            //     setSquatsState("");
+            // }
+            return;
         }
 
         // Squat state: standing --> squat = 1 squat
-        const leftAngle = angle(squatCoords["leftKnee"], squatCoords["leftHip"], squatCoords["rightHip"]);
-        const rightAngle = angle(squatCoords["rightKnee"], squatCoords["rightHip"], squatCoords["leftHip"]);
+        const left = angle(squatCoords["leftKnee"].position, squatCoords["leftHip"].position, squatCoords["rightHip"].position);
+        const right = angle(squatCoords["rightKnee"].position, squatCoords["rightHip"].position, squatCoords["leftHip"].position);
 
-        setLeftThighAngle(leftAngle);
-        setRightThighAngle(rightAngle);
-
-        if ((squatsState === "" || squatsState === "down") && checkSquatStanding(leftAngle) && checkSquatStanding(rightAngle)) {
+        setleftSquatAngle(left)
+        setrightSquatAngle(right)
+        
+        if (squatsState === "down" && (checkSquatStanding(left) && checkSquatStanding(right))) {
             setSquatsState("up");
-        } else if (squatsState === "up" && checkSquatDown(leftAngle) && checkSquatDown(rightAngle)) {
+        } else if (squatsState === "up" && (checkSquatDown(left) && checkSquatDown(right))) {
             setSquatsState("down");
             setSquatsCount(squatsCount + 1);
+        } else {
+            return true;
         }
+
+        return false;
     }
 
+    const curlsDetection = (curlCoords) => {
+        // User will be sideways when doing curls. We should expect to see at least one side.
+        const leftShoulderCoord = curlCoords["leftShoulder"],
+              leftElbowCoord = curlCoords["leftElbow"],
+              rightShoulderCoord = curlCoords["rightShoulder"],
+              rightElbowCoord = curlCoords["rightElbow"],
+              leftWristCoord = curlCoords["leftWrist"],
+              rightWristCoord = curlCoords["rightWrist"];
+
+        if (!((leftShoulderCoord && leftElbowCoord && leftWristCoord) || (rightShoulderCoord && rightElbowCoord && rightWristCoord))) {
+            // only reset if currently on resting position 
+            setCurlsState("");
+            return;
+        }
+
+        // choose side with the higher confidence
+        let leftConfidence = 0, rightConfidence = 0; 
+
+        if (leftShoulderCoord && leftElbowCoord && leftWristCoord) {
+            // verify they are in order
+            leftConfidence = average([leftShoulderCoord.score, leftElbowCoord.score], leftWristCoord.score);
+        }
+        if (rightShoulderCoord && rightElbowCoord && rightWristCoord) {
+            rightConfidence = average([rightShoulderCoord.score, rightElbowCoord.score], rightWristCoord.score);
+        }
+
+        const leftSideStatus = leftConfidence > rightConfidence;
+        const curlAngle = leftSideStatus ? 
+            angle(leftShoulderCoord.position, leftElbowCoord.position, leftWristCoord.position) :
+            angle(rightShoulderCoord.position, rightElbowCoord.position, rightWristCoord.position)
+
+        if (leftSideStatus) {
+            setleftCurlAngle(curlAngle);
+        } else {
+            setrightCurlAngle(curlAngle);
+        }
+        
+        if ((curlsState === "" || curlsState === "curl") && checkCurlRest(curlAngle)) {
+            setCurlsState("rest");
+        } else if (curlsState === "rest" && checkCurlActive(curlAngle)) {
+            setCurlsState("curl");
+            setCurlsCount(curlsCount + 1);
+        }
+    }
 
     const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
@@ -59,15 +123,16 @@ const PoseEstimation = () => {
 
         poseNet.on("pose", (results) => {
             setPoses(results);
-
-            // squatDetection(results);
         });
 	};
 
 	const draw = (p5) => {
         p5.image(videoRef.current, 0, 0, 640, 480)
 
-        const squatCoords = {} 
+        const squatCoords = {};
+        const curlCoords = {};
+
+        const allKeypoints = {};
 
         // draws keypoints
         for (let i = 0; i < poses.length; i++) {
@@ -78,15 +143,20 @@ const PoseEstimation = () => {
                 const keypoint = pose.keypoints[j];
                 // Only draw an ellipse is the pose probability is bigger than 0.2
                 if (keypoint.score > minPoseConfidence) {
-                    p5.fill(255, 0, 0);
+                    p5.fill(255, 255, 255);
                     p5.noStroke();
                     p5.ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
 
-                    if (keypoint.part === "leftKnee" || keypoint.part === "leftHip" || keypoint.part === "rightKnee" || keypoint.part === "rightHip") {
-                        squatCoords[keypoint.part] = keypoint.position;
+                    if (squatJoints.some((part) => part === keypoint.part)) {
+                        squatCoords[keypoint.part] = keypoint;
                     }
-                }
 
+                    if (curlJoints.some((part) => part === keypoint.part)) {
+                        curlCoords[keypoint.part] = keypoint;
+                    }
+
+                    allKeypoints[keypoint.part] = keypoint.position;
+                }
             }
         }
 
@@ -97,21 +167,45 @@ const PoseEstimation = () => {
             for (let j = 0; j < skeleton.length; j += 1) {
                 const partA = skeleton[j][0];
                 const partB = skeleton[j][1];
-                p5.stroke(255, 0, 0);
+                p5.stroke(255, 255, 255);
                 p5.line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
             }
         }
 
         squatDetection(squatCoords);
+        curlsDetection(curlCoords);
+
+        // if (!currentExercise) {
+        //     // finds starting position
+        //     const exercise = findExercise(allKeypoints);
+        //     setCurrentExercise(exercise);
+        // }
+
+        // if (currentExercise === "squat") {
+        //     const fetchNewExercise = squatDetection(squatCoords);
+
+        //     if (fetchNewExercise && squatsState === "up") {
+        //         const exercise = findExercise(allKeypoints);
+        //         console.log(exercise);
+        //     }
+        // }      
+
+        // curlsDetection(curlCoords);
 	};
 
     return (
         <div>
+            <h3>Current Exercise: {currentExercise}</h3>
             <Sketch setup={setup} draw={draw} />;
-            <p>Left Thigh Angle: {leftThighAngle}</p>
-            <p>Right Thigh Angle: {rightThighAngle}</p>
-            <p>Number of Squats: {squatsCount}</p>
-            <p>Squats State: {squatsState}</p>
+            <h3>Number of Squats: {squatsCount}</h3>
+            <h3>Squats State: {squatsState}</h3>
+            <h3>Squats Left Angle: {leftSquatAngle}</h3>
+            <h3>Squats Right Angle: {rightSquatAngle}</h3>
+            <br></br>
+            <h3>Number of Curls: {curlsCount}</h3>
+            <h3>Curls State: {curlsState}</h3>
+            <h3>Curls Left Angle: {leftCurlAngle}</h3>
+            <h3>Curls Right Angle: {rightCurlAngle}</h3>
         </div>
     )
 }
--- frontend/src/components/VideoCall/PoseEstimation/utils.js
+++ frontend/src/components/VideoCall/PoseEstimation/utils.js
@@ -8,4 +8,55 @@ export const angle = (P1, P2, P3) => {
 }
 
 export const checkSquatStanding = (angle) => angle > 65 && angle < 100;
-export const checkSquatDown = (angle) => angle > 140;
+export const checkSquatDown = (angle) => angle > 135;
+
+export const checkCurlRest = (angle) => angle > 140 && angle < 170; 
+export const checkCurlActive = (angle) => angle < 60 && angle !== 0;
+
+export const average = (listOfNums) => listOfNums.reduce((acc, val) => {
+  acc += val;
+  return acc;
+}, 0)/listOfNums.length;
+
+
+export const isSquatPosition = (keypoints) => {
+  // check if upper body is square --> squat position
+  let leftSide = [keypoints["leftHip"], keypoints["leftShoulder"], keypoints["rightShoulder"]]
+  let rightSide = [keypoints["leftHip"], keypoints["rightHip"], keypoints["rightShoulder"]]
+
+  leftSide = leftSide.filter((x) => x);
+  rightSide = rightSide.filter((x) => x);
+
+  if (leftSide.length !== 3 || rightSide.length !== 3) {
+    return false;
+  }
+
+  const leftSideAngle = angle(leftSide[0], leftSide[1], leftSide[2]);
+  const rightSideAngle = angle(rightSide[0], rightSide[1], rightSide[2]);
+
+  // make sure they're about right angle
+  if (leftSideAngle > 100 || leftSideAngle < 80 || rightSideAngle > 100 || rightSideAngle < 80) {
+    return false; 
+  }
+
+  return true;
+}
+
+export const isCurlPosition = (keypoints) => {
+  // left/right shoulder are short distance, since user will be sitting from the side
+  const leftShoulder = keypoints["leftShoulder"];
+  const rightShoulder = keypoints["rightShoulder"];
+
+  if (!leftShoulder || !rightShoulder) {
+    return false; 
+  }
+};
+
+export const findExercise = (keypoints) => {
+  
+  if (isSquatPosition(keypoints)) {
+    return "squat";
+  }
+
+  return undefined
+}
